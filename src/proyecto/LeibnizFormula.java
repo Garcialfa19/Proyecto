dcaf035d4177c066123c06628325aaba96d1a453
David <garcialfa19@icloud.com>
2020-10-30 17:58:42 -0600

    Second Commit

--- src/proyecto/LeibnizFormula.java
+++ src/proyecto/LeibnizFormula.java
@@ -3,7 +3,13 @@ package proyecto;
 public class LeibnizFormula {
 
     public static void main(String[] args){
-        System.out.println("leibniz");
+
+        double pi = 0;
+        for (int i = 1; i <1000000000; i += 4) {
+            pi += 8.0 / (i * (i + 2l));
+        }
+        System.out.println(pi);
+
     }
 
 
