60f6dbd34783318ab031466e29a97f48e3acd648
David <garcialfa19@icloud.com>
2020-11-05 18:03:18 -0600

    Eighth Commit

--- src/proyecto/LeibnizFormula.java
+++ src/proyecto/LeibnizFormula.java
@@ -2,12 +2,22 @@ package proyecto;
 
 public class LeibnizFormula {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        int count = 1000000000;
+        double pi = 0;
+        double denominator = 1;
 
-        //not done YET
+        for (int x = 0; x < count; x++) {
 
+            if (x % 2 == 0) {
+                pi = pi + (1 / denominator);
+            } else {
+                pi = pi - (1 / denominator);
+            }
+            denominator = denominator + 2;
+        }
+        pi = pi * 4;
+        System.out.println(pi);
     }
-
-
 }
